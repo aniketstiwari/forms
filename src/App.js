47745b41155c1a5a96b42edd7c1cd5e309558c76
anikett-rpx <anikett_c@rpxcorp.com>
2020-09-24 18:35:40 +0530

    Added text field for components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+
+  const [name, setName] = useState("");
+  const [fullName, setFullName] = useState();
+
+  const inputEvent = (event) => {
+    setName(event.target.value);
+  }
+
+  const onSubmit = () => {
+    setFullName(name);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>Hello {fullName} </h1>
+      <input type="text" placeholder="Enter your Name" onChange={inputEvent} value={name}/>
+      <button onClick={onSubmit}>Submit</button>
+    </>
+  )
 }
 
 export default App;
